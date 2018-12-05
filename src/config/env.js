d1bd29f8ea091d5636b53b372b479303b0a3b16a
messaoudighada5 <messaoudighada5@gmail.com>
2018-12-05 16:24:44 +0100

    initial commit

---
+++ src/config/env.js
@@ -0,0 +1,6 @@
+export const NODE_ENV = process.env.NODE_ENV || "development";
+export const PORT = process.env.PORT || 3000;
+export const DB =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/archive";
+export const SECRET =
+  process.env.SECRET || "thisisaverysecurestringtonotbeinghacked";
---
+++ src/config/routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+const router = express.Router();
+
+import userRoute from "../components/user/userRoutes";
+// import documentRoute from '../components/document/documentRoutes'
+// import majorRoute from '../components/major/majorRoutes'
+// import correctionRoute from '../components/correction/correctionRoutes'
+// import subjectRoute from '../components/subject/subjectRoutes'
+
+userRoute(router);
+// documentRoute(router)
+// majorRoute(router)
+// correctionRoute(router)
+// subjectRoute(router)
+
+export default router;
