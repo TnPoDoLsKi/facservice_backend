c7f07233ed1be8ef6bb0065978e259b44c96998a
Louay <atoui.louay77@gmail.com>
2018-12-05 20:22:11 +0100

    add middlewares + sign in

--- src/components/user/userRoutes.js
+++ src/components/user/userRoutes.js
@@ -1,10 +1,12 @@
-import { getByType, isAdmin, createUser, updateUser, deleteUser, signIn, signOut } from "./userController";
+import { getByType, createUser, updateUser, deleteUser, signIn, signOut } from "./userController";
+import { isLoggedIn } from "../../services/middlewares/isLoggedIn";
+import { isAdmin } from "../../services/middlewares/isAdmin";
 
 export default function(router) {
-    router.get("/api/users/:type", isAdmin, getByType);
-    router.post("/api/users/signup", createUser);
-    router.put("/api/users/:id", updateUser);
-    router.delete("/api/users/:id",  deleteUser);
+    router.get("/api/users/:type",isLoggedIn, isAdmin, getByType);
+    router.post("/api/users/signup", isLoggedIn, createUser);
+    router.put("/api/users/:id",isLoggedIn, updateUser);
+    router.delete("/api/users/:id",isLoggedIn,  deleteUser);
     router.post('/api/auth/signin', signIn);
     router.post('/api/auth/signout', signOut)
 }
