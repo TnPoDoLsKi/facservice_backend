ff1acff2c27859974186936335931f0a2e55d263
Messaoudi Ghada <messaoudi.ghada@issatso.u-sousse.tn>
2018-12-17 21:21:06 +0100

    fix eslint error from userManagement branch

--- src/components/user/userController.js
+++ src/components/user/userController.js
@@ -4,25 +4,33 @@ import { SECRET } from "../../config/env";
 import jwt from "jsonwebtoken";
 
 export async function createUser(req, res) {
-    try{
-        let user = _.pick(req.body, 'email', 'hashedPassword', 'type', 'firstName', 'lastName', 'avatar');
+  try {
+    let user = _.pick(
+      req.body,
+      "email",
+      "hashedPassword",
+      "type",
+      "firstName",
+      "lastName",
+      "avatar"
+    );
 
-        await User.findOne( { email: user.email }, function (err, user) {
-            if (err) {
-                return res.status(500).end()
-            }
-            else if (user) {
-                return res.status(208).end()
-            }
-        });
+    await User.findOne({ email: user.email }, function(err, user) {
+      if (err) {
+        return res.status(500).end();
+      } else if (user) {
+        return res.status(208).end();
+      }
+    });
 
-        await User.create(user).populate('major').exec();
+    await User.create(user)
+      .populate("major")
+      .exec();
 
-        return res.status(201).end()
-
-    } catch(err) {
-        res.status(500).end()
-    }
+    return res.status(201).end();
+  } catch (err) {
+    res.status(500).end();
+  }
 }
 
 export async function getByType(req, res) {
@@ -45,20 +53,17 @@ export async function getByType(req, res) {
 }
 
 export async function getAll(req, res) {
-    try {
+  try {
+    let users = await User.find()
+      .populate("major")
+      .exec();
 
-        let users = await User.find().populate('major').exec();
-
-        return res.status(200).json(
-            users
-        );
-
-    } catch(err) {
-        console.log(err);
-        return res.status(500).end()
-    }
+    return res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).end();
+  }
 }
-
 
 export async function updateUser(req, res) {
   try {
@@ -108,26 +113,24 @@ export async function deleteUser(req, res) {
 }
 
 export async function signIn(req, res) {
-    try{
-        await User.findOne( { email: req.body.email }, function (err, user) {
-            if(!user) {
-                return res.status(400).end()
-            }
-            user.comparePassword(req.body.hashedPassword, function (err, equal) {
-                if(equal && !err){
-                    let token = jwt.sign(user.toJSON(), SECRET, {expiresIn: 250000});
-                    //req.session.
-                    return res.json({"JWT": token})
-                }
-                else{
-                    return res.status(400).end()
-                }
-            })
-        })
-    } catch (err) {
-        return res.status(500).end()
-    }
-
+  try {
+    await User.findOne({ email: req.body.email }, function(err, user) {
+      if (!user) {
+        return res.status(400).end();
+      }
+      user.comparePassword(req.body.hashedPassword, function(err, equal) {
+        if (equal && !err) {
+          let token = jwt.sign(user.toJSON(), SECRET, { expiresIn: 250000 });
+          //req.session.
+          return res.json({ JWT: token });
+        } else {
+          return res.status(400).end();
+        }
+      });
+    });
+  } catch (err) {
+    return res.status(500).end();
+  }
 }
 
 export async function signOut(req, res) {}
--- src/services/middlewares.js
+++ src/services/middlewares.js
@@ -1,30 +1,27 @@
 import jwt from "jsonwebtoken";
-import {SECRET} from "../config/env";
+import { SECRET } from "../config/env";
 import atob from "atob";
 
 export function isLoggedIn(req, res, next) {
-    if ('authorization' in req.headers) {
-        let bearer = req.headers['authorization'];
-        let token = bearer.split(' ')[1];
+  if ("authorization" in req.headers) {
+    let bearer = req.headers["authorization"];
+    let token = bearer.split(" ")[1];
 
-        if (!token) {
-
-            return res.status(403).send({
-                auth: false,
-                message: 'No token provided'
-            })
-        }
-        else {
-            jwt.verify(token, SECRET, (err, tokenDecoded) => {
-                if (err)
-                    return res.status(500).send();
-                console.log(tokenDecoded);
-                next();
-            })
-        }
+    if (!token) {
+      return res.status(403).send({
+        auth: false,
+        message: "No token provided"
+      });
     } else {
-        return res.status(400).end()
+      jwt.verify(token, SECRET, (err, tokenDecoded) => {
+        if (err) return res.status(500).send(err);
+        console.log(tokenDecoded);
+        next();
+      });
     }
+  } else {
+    return res.status(400).end();
+  }
 }
 
 export function isAdmin(req, res, next) {
