491a886e5d57ed86b22d804f57f2cc25b5e4ef43
Messaoudi Ghada <messaoudighada5@gmail.com>
2019-01-29 23:41:41 +0100

    fix path in production

--- src/services/uploadService.js
+++ src/services/uploadService.js
@@ -33,9 +33,12 @@ export async function upload(req, res) {
     */
   const _fileHandler = (name, file) => {
     const host = req.protocol + "://" + req.headers.host;
-    console.log(req.headers.host)
-    const url = host + "/uploads/" + file.path.split("\\").pop();
-    console.log(file.path)
+    let url = "";
+    if (req.headers.host === "igc.tn:3005") {
+      url = host + "/uploads/" + file.path.split("//").pop();
+    } else {
+      url = host + "/uploads/" + file.path.split("\\").pop();
+    }
     fileUrls.push(url);
     return fileUrls;
   };
