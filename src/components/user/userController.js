30568e1610fe129336c90cf7f42e7b4fae5ac89c
Wael Ben Taleb <waelben7@gmail.com>
2019-02-19 18:20:26 +0100

    fix

--- src/components/user/userController.js
+++ src/components/user/userController.js
@@ -111,7 +111,7 @@ export async function getAll(req, res) {
 
 export async function getCurrent(req, res) {
   try {
-    const user = _.pick(req.user, ["firstName", "lastName", "email"])
+    const user = _.pick(req.user, ["_id", "firstName", "lastName", "email"])
     return res.status(200).json(user);
   } catch (err) {
     return res.status(500).end();
