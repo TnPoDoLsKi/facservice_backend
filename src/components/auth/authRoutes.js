6d4016fd9f5963456d774eeaa42c2ac4b52c6eb6
Messaoudi Ghada <messaoudighada5@gmail.com>
2019-03-15 00:05:07 +0100

    fix function name

--- src/components/auth/authRoutes.js
+++ src/components/auth/authRoutes.js
@@ -1,8 +1,8 @@
-import { create, signIn, signOut, testMailer, activeAccount } from "./authController";
+import { signUp, signIn, signOut, testMailer, activeAccount } from "./authController";
 import {isLoggedIn} from "../../services/middlewares"
 
 export default function (router) {
-  router.post("/auth/signup", create);
+  router.post("/auth/signup", signUp);
   router.post("/auth/signin", signIn);
   router.get("/mailing", testMailer);
   router.get("/activate/:token", activeAccount);
