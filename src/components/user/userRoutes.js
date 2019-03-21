71bf466e2764301f016f9e4cc2e10e65e6deda71
SofienMsaddak <sofien.msddak@gmail.com>
2019-03-21 02:40:43 +0100

    removed isLoggedIn for testing

--- src/components/user/userRoutes.js
+++ src/components/user/userRoutes.js
@@ -8,7 +8,7 @@ import {
 import { isLoggedIn, isAdmin } from "../../services/middlewares";
 
 export default function(router) {
-  router.get("/user", isLoggedIn, getCurrent);
+  router.get("/user", getCurrent);
   router.get("/users", isLoggedIn, isAdmin, getAll);
   router.get("/users/:type", isLoggedIn, isAdmin, getByType);
 
