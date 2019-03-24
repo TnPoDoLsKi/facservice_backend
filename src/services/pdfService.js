f4e489d13ac2636497d8953d8b5402a2ad311382
Messaoudi Ghada <messaoudighada5@gmail.com>
2019-03-24 22:02:28 +0100

    serve /pdfs

--- src/services/pdfService.js
+++ src/services/pdfService.js
@@ -7,30 +7,40 @@ export async function convert(req, res) {
 
     var printer = new PdfPrinter();
 
-    req.body.filesStaging.forEach(function (imagePath, index) {
+    await req.body.filesStaging.forEach(async function(imagePath, index) {
       content[index] = {
-        image: "public" + imagePath.replace("http://localhost:3000", ""),
+        image: "public" + imagePath.replace("http://igc.tn:3005", ""),
         width: 595,
         height: 842
-      }
-    })
+      };
+    });
 
     var docDefinition = {
       content: content,
+      info: {
+        title: req.body.documentTitle,
+        author: "IGC"
+      },
       pageMargins: [0, 0, 0, 0]
     };
 
     var pdfDoc = printer.createPdfKitDocument(docDefinition);
     pdfDoc.pipe(
       fs.createWriteStream(
-        "public/pdfs/" + req.body.documentTitle.replace(/ /g, "_")
-      )
+        "public/pdfs/" + req.body.documentTitle.replace(/ /g, "_") + ".pdf"
+      ),
+      {
+        encoding: "utf16"
+      }
     );
     pdfDoc.end();
 
     var filePath =
-      "http://igc.tn:3005/public/pdfs/" +
-      req.body.documentTitle.replace(/ /g, "_");
+      req.headers.host === "igc.tn:3005"
+        ? "http://igc.tn:3005/pdfs/"
+        : "http://localhost:3000/pdfs/" +
+          req.body.documentTitle.replace(/ /g, "_") +
+          ".pdf";
     res.json(filePath);
   } catch (error) {
     console.log(error);
