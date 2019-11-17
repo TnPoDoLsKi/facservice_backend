c55f4bcc65c092b4e4b84ad5b347f63684150778
Wael Ben Taleb <waelben7@gmail.com>
2019-11-17 15:01:12 +0100

    database name

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,6 @@
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const PORT = process.env.PORT || 3000;
 export const DB =
-  process.env.MONGODB_URI || "mongodb://archive:uZp7cO$1$@51.38.69.62:27017/archive";
+  process.env.MONGODB_URI || "mongodb://facservice:uZp7cO$1$@51.38.69.62:27017/facservice";
 export const SECRET =
   process.env.SECRET || "thisisaverysecurestringtonotbeinghacked";
