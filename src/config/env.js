c7ec86ceaeac9841e017fcd8d4f52d7be8a1e258
Wael Ben Taleb <waelben7@gmail.com>
2019-11-18 23:55:46 +0100

    fix

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,6 @@
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const PORT = process.env.PORT || 3000;
 export const DB =
-  process.env.MONGODB_URI || "mongodb://facservice:uZp7cO$1$@51.38.69.62:27017/facservice";
+  process.env.MONGODB_URI || "mongodb://facservice:uZp7cO$1$@localhost:27017/facservice";
 export const SECRET =
   process.env.SECRET || "thisisaverysecurestringtonotbeinghacked";
