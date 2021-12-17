cf2033027df28ec06bbb0b1df7a55d4dad05c016
Masmoudi1 <masmoudiousema9@gmail.com>
2021-12-17 15:30:37 +0100

    inti

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const PORT = process.env.PORT || 3000;
 export const DB =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/facservice";
+  process.env.MONGODB_URI || "mongodb+srv://facservice:facservice@cluster0.fuo82.mongodb.net/facservice?retryWrites=true&w=majority";
 export const SECRET =
   process.env.SECRET || "thisisaverysecurestringtonotbeinghacked";
 export const HOST =
